4ec007bae18b748b4b16f84e3b62412ce220f427
Nhung NGUYEN <nnguyen@unowhy.com>
2026-03-21 15:06:16 +0100

    Colleen: add hello code and Makefile

---
+++ Colleen/ASM/Colleen.s
@@ -0,0 +1,15 @@
+section .rodata
+    hello_msg db "Hello world", 0
+    hello_msg_len equ $ - hello_msg
+section .text
+    global _start
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, hello_msg
+    mov rdx, hello_msg_len
+    syscall
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
