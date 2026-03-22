88aa415a4bae81a3dbca4d37d63483564f986bdc
Nhung NGUYEN <nnguyen@unowhy.com>
2026-03-22 10:37:21 +0100

    Colleen: add asm code

--- Colleen/ASM/Colleen.s
+++ Colleen/ASM/Colleen.s
@@ -1,15 +1,17 @@
+; dr
 section .rodata
-    hello_msg db "Hello world", 0
-    hello_msg_len equ $ - hello_msg
+    msg db "; dr%1$csection .rodata%1$c    msg db %2$c%3$s%2$c, 0%1$csection .text%1$c    extern printf%1$c    global main%1$cmain:%1$c; quine%1$c    lea rdi, [rel msg]%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    lea rcx, [rel msg]%1$c    xor rax, rax%1$c    call printf wrt ..plt%1$c    xor rax, rax%1$c    ret%1$csection .note.GNU-stack noalloc noexec nowrite progbits", 0
 section .text
-    global _start
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, hello_msg
-    mov rdx, hello_msg_len
-    syscall
-
-    mov rax, 60
-    mov rdi, 0
-    syscall
+    extern printf
+    global main
+main:
+; quine
+    lea rdi, [rel msg]
+    mov rsi, 10
+    mov rdx, 34
+    lea rcx, [rel msg]
+    xor rax, rax
+    call printf wrt ..plt
+    xor rax, rax
+    ret
+section .note.GNU-stack noalloc noexec nowrite progbits
